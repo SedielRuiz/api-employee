346a75fb58bac4abc12b066158f3d8428ce512a3
mariapaulav03 <14mariapaula09@gmail.com>
2024-04-03 13:58:21 -0500

    :tada: Complete information in Create Employees

--- src/domain/migrations/1711598827339-employees.ts
+++ src/domain/migrations/1711598827339-employees.ts
@@ -13,6 +13,13 @@ export class CreateEmployeesTable1612345678901 implements MigrationInterface {
                 "phone" character varying NOT NULL,
                 "address" character varying NOT NULL,
                 "position" character varying NOT NULL,
+                "status" boolean NOT NULL,
+                "department" character varying,
+                "salary" integer,
+                "hireDate" TIMESTAMP,
+                "supervisor" character varying,
+                "emergencyContactName" character varying,
+                "emergencyContactPhone" character varying,
                 "created_at" TIMESTAMP NOT NULL DEFAULT now(),
                 "updated_at" TIMESTAMP NOT NULL DEFAULT now(),
                 CONSTRAINT "PK_b9535a98350d5b26e7eb0c26af4" PRIMARY KEY ("id")
